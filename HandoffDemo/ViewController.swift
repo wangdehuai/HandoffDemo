606a9c63b24485d9cbd3ecff9bb0931f5f72d407
Gabriel Theodoropoulos <gabriel@Gabriels-Mac-mini.local>
2014-11-17 17:51:37 +0200

    Initial Commit

---
+++ HandoffDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandoffDemo
+//
+//  Created by Gabriel Theodoropoulos on 17/11/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
